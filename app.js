e0e54d9442ff402856a04c141d50501f256ceca6
Evanilson Pereira <pevanilson99@gmail.com>
2024-02-21 10:28:24 -0300

    Creating routes file

--- app.js
+++ app.js
@@ -3,10 +3,9 @@ const express = require('express');
 const app = express();
 const PORT = 5000 || process.env.PORT;
 
-app.get('/', (req, res) => {
-    res.send('Welcome!');
-});
+app.set('view', 'engine');
 
+app.use(require('./routes/main.js'));
 
 app.listen(PORT, () => {
     console.log(`Up and running on PORT ${PORT}`);
---
+++ routes/main.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.render('signup');
+});
+
+router.get('/login', (req, res) => {
+    res.render('login');
+});
+
+module.exports = router;
