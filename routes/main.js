fb2c548930e035228ea26c44fd290da091af826c
Evanilson Pereira <pevanilson99@gmail.com>
2024-02-21 10:39:44 -0300

    Creating login ejs page

--- routes/main.js
+++ routes/main.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
+// Login
 router.get('/', (req, res) => {
-    res.render('signup');
+    res.render('login');
 });
 
-router.get('/login', (req, res) => {
-    res.render('login');
+// Signup
+router.get('/signup', (req, res) => {
+    res.render('signup');
 });
 
 module.exports = router;
